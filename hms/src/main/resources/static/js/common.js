89d41b9f7f4f1fdf87bf5e39b70425eed15f1a15
crizefish <1032936569@qq.com>
2018-01-17 11:58:04 +0800

    dd

--- hms/src/main/resources/static/js/common.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-	$("#search_button").click(function sub(){
-		var keyWord = $("#keyWord").val();
-		if(keyWord=="请输入关键字" || keyWord == ""){
-			window.location.href="/other/index";
-		}else{
-			window.location.href="/other/search"+"?keyWord="+keyWord;
-		}
-	});
